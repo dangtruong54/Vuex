1d630293263466c685f9eb3918aee3ba76836069
xa10-1 <truong.dang@tripath.vn>
2019-09-30 17:10:58 +0700

    Vuex counter

---
+++ my-task-vuex/src/components/Counter.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="counter">
+    <!--goi state boi: $store.state.nameState-->
+    Clicked: {{ $store.state.count }} times, count is {{ evenOrOdd }}.
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementOdd">Increment if odd</button>
+    <button @click="decrementEven">Decrement if even</button>
+  </div>
+</template>
+
+<script>
+  import { mapGetters, mapActions } from 'vuex'
+  import { mapState } from 'vuex'
+  export default {
+    name: "Counter",
+    computed:
+      mapGetters([
+      'evenOrOdd'
+    ]),
+
+    // methods: mapActions([
+    //     'increment',
+    //     'decrement'
+    // ])
+    methods: {
+      increment(e) {
+        e.preventDefault();
+        // dispatch call action -> action call mutation -> update state
+        this.$store.dispatch('increment')
+      },
+      decrement(e) {
+        e.preventDefault();
+        this.$store.dispatch('decrement')
+      },
+      incrementOdd(e) {
+        e.preventDefault();
+        this.$store.dispatch('incrementOdd')
+      },
+      decrementEven(e) {
+        e.preventDefault();
+        this.$store.dispatch('decrementEven')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- my-task-vuex/src/store/store.js
+++ my-task-vuex/src/store/store.js
@@ -1,18 +1,47 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
 Vue.use(Vuex)
+// Khai bao state cho store
+const state = {
+  count: 0
+}
 
-import getters from './getters'
-import mutations from './mutations'
-import actions from './actions'
+// Khai bao mutations cho store
+
+const mutations = {
+  increment(state) {
+    state.count ++
+  },
+  decrement(state) {
+    state.count --
+  },
+  changeCount(state) {
+    if(state.count % 2 === 0) {
+      state.count ++
+    }else {
+      state.count --
+    }
+  }
+}
+
+// Khai bao action cho store
+
+const actions = {
+  increment: ({commit}) => commit('increment'),
+  decrement: ({commit}) => commit('decrement'),
+  incrementOdd: ({commit, state}) => commit('changeCount'),
+  decrementEven: ({commit, state}) => commit('changeCount')
+}
+
+// Khai bao getter cho state
+
+const getters = {
+  evenOrOdd: state => state.count % 2 ? 'even' : 'odd'
+}
 
 export default new Vuex.Store({
-  state: {
-    tasks: [],
-    newTask: ''
-  },
-  getters,
+  state,
   mutations,
-  actions
+  actions,
+  getters
 })
